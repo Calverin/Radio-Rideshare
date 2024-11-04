ccd8f540575db69ed43010d5b2254e707ba65eae
Calverin <38361803+Calverin@users.noreply.github.com>
2024-11-04 16:54:07 -0500

    Re-added car control changes (spindrifts)

--- Scripts/car.gd
+++ Scripts/car.gd
@@ -7,6 +7,7 @@ var current_lane: int
 var drifting: bool = false
 var drift_time: int = 0
 var drift_direction: float = 0
+var turn_offset: float = 0
 
 func _ready():
 	current_lane = ceilf(lanes / 2.0)
@@ -14,50 +15,71 @@ func _ready():
 
 func _physics_process(delta: float):
 	## Drifting
-	if Input.is_action_just_pressed("drift_left") and not drifting:
-		drifting = true
-		drift_direction = -0.5
-	if Input.is_action_just_pressed("drift_right") and not drifting:
-		drifting = true
-		drift_direction = 0.5
 	if drifting:
 		drift(delta)
 	else:
-		$Body.rotation.y = lerpf($Body.rotation.y, 0, delta * 8)
-		if Input.is_action_just_pressed("quick_left"):
-			$Body.rotation.y += 0.25
-			switch_lane(-1)
-		if Input.is_action_just_pressed("quick_right"):
-			$Body.rotation.y -= 0.25
-			switch_lane(1)
-			
-		
+		inputs()
+	if turn_offset != 0:
+		turn_offset = lerpf(turn_offset, 0, delta * 16)
 	
 	## Movement
-	position.x = lerpf(position.x, current_lane * 10, delta * 20)
-	$Body.position.y = lerpf($Body.position.y, 0, delta * 20)
+	position.x = lerpf(position.x, current_lane * 10, delta * 16)
 	position.z -= speed * delta
-	
+	## Movement VFX
+	$Body.position.y = lerpf($Body.position.y, 0, delta * 20)
+	$Body.rotation.y = lerpf($Body.rotation.y + clampf(turn_offset, -1, 1), $Body.rotation.y - (fmod($Body.rotation.y, (2 * PI)) if abs(turn_offset) < 0.1 else 0) - drift_direction, delta * 8)
+
+# -1 == left, 1 == right
 func drift(delta: float):
 	## Params
-	var axis = Input.get_axis("steer_left", "steer_right")
-	# -1 == left, 1 == right
-	drift_direction = -1 if axis < 0 else 1 if axis > 0 else drift_direction
 	drift_time += 1
 	
-	## VFX
-	$Body.rotation.y = lerpf($Body.rotation.y, -drift_direction, delta * 5)
-	# Early Drift "Hop"
+	## Visual Rotation
+	#$Body.rotation.y = lerpf($Body.rotation.y, -drift_direction, delta * 5)
+	
+	## Start of Drift
 	if drift_time < 10:
+		# Early Drift "Hop"
 		$Body.position.y = lerpf($Body.position.y, 1, delta * 30)
+		# Spin Turn
+		var spin = Input.get_axis("quick_left", "quick_right")
+		if drift_direction > 0 and spin < 0:
+			switch_lane(-2)
+			turn_offset -= 2 * PI
+		if drift_direction < 0 and spin > 0:
+			switch_lane(2)
+			turn_offset += 2 * PI
+
 	## Ending
-	if not (Input.is_action_pressed("drift_left") or Input.is_action_pressed("drift_right")):
-		drifting = false
-		drift_time = 0
+	if (drift_direction < 0 and not Input.is_action_pressed("drift_left"))    \
+		or (drift_direction > 0 and not Input.is_action_pressed("drift_right")):
 		switch_lane(drift_direction)
-		
+
 func switch_lane(dir):
-	if dir < 0 and current_lane > 1:
-		current_lane -= 1
-	if dir > 0 and current_lane < lanes:
-		current_lane += 1
+	drifting = false
+	drift_time = 0
+	drift_direction = 0
+	var lane_change = ceil(abs(dir))
+	if dir < 0 and current_lane > lane_change:
+		current_lane -= lane_change
+		return
+	if dir > 0 and current_lane <= lanes - lane_change:
+		current_lane += lane_change
+		return
+	# Try again
+	if lane_change > 1:
+		switch_lane(dir / 2)
+
+func inputs():
+	if Input.is_action_just_pressed("drift_left"):
+		drifting = true
+		drift_direction = -1
+	elif Input.is_action_just_pressed("drift_right"):
+		drifting = true
+		drift_direction = 1
+	elif Input.is_action_just_pressed("quick_left"):
+		turn_offset = 0.1
+		switch_lane(-1)
+	elif Input.is_action_just_pressed("quick_right"):
+		turn_offset = -0.1
+		switch_lane(1)
