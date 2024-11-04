e707717edc34d2a73f7907c286dbb047dd6c5aeb
Noam Ben Simon <noampvb@gmail.com>
2024-11-04 17:23:45 -0500

    Updated settings to now be visible in git

--- Scripts/settings.gd
+++ Scripts/settings.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
