4817bf4ad2b769b208d5dfab4ad52b8b7ec3b3d0
Calverin <38361803+Calverin@users.noreply.github.com>
2024-12-18 17:45:35 -0500

    Loading objects when opening levels, +/- lanes

--- Scripts/level_editor.gd
+++ Scripts/level_editor.gd
@@ -9,27 +9,60 @@ var level_length: int
 @onready var LOADER = $Loader
 @onready var GROUND = $Level/Ground.mesh
 @onready var HIGHLIGHT: MeshInstance3D = $Level/Highlight
-@onready var NOTES = $Level/Notes
+@onready var OBJECTS = $Level/Objects
 
 var TAP_NOTE = preload("res://Scenes/Notes/tap_note.tscn")
 var PARKED_CAR = preload("res://Scenes/Obstacles/parked_car.tscn")
 
 func _ready() -> void:
+	var lanes = $UI/RightSidebar/Container/Lanes
+	lanes.get_node("Add").pressed.connect(_on_add_lane_pressed.bind(lanes.get_node("Amount")))
+	lanes.get_node("Subtract").pressed.connect(_on_subtract_lane_pressed.bind(lanes.get_node("Amount")))
+	reload_level()
+	
+func reload_level() -> void:
+	# Loading data
+	var starting_tile = Vector2i(current_tile)
+	clear_objects()
 	var level = LevelLoader.current_level_name
 	if not level:
 		return
-	print("Starting to edit level: " + level)
+	print("(Re)Loading level editor with: " + level)
 	current_level = LOADER.load_level(level)
 	
 	var lanes = current_level.lanes
+	$UI/RightSidebar/Container/LevelName.text = current_level.title
+	$UI/RightSidebar/Container/Lanes/Amount.text = str(lanes)
+	if (not starting_tile):
+		starting_tile = Vector2i((lanes / 2), 0)
 	GROUND.size.x = lanes * 10
 	GROUND.material.uv1_scale.x = lanes
 	
-	current_tile = Vector2i((lanes / 2), 0)
 	
 	level_data = current_level.data
 	level_length = len(level_data)
+	
+	# Insert initial objects
+	for i in range(level_length):
+		var line = level_data[i].split()
+		for o in range(len(line)):
+			if line[o] != '0':
+				current_tile = Vector2i(o, i)
+			if line[o] == '1':
+				insert_object(TAP_NOTE, 1)
+			if line[o] == '2':
+				insert_object(PARKED_CAR, 2)
 
+	current_tile = Vector2i(starting_tile)
+	
+func clear_objects():
+	for child in OBJECTS.get_children():
+		OBJECTS.remove_child(child)
+		child.free()
+	#if len(OBJECTS.get_children()) > 0:
+	#	clear_objects()
+	
+	
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_up"):
 		current_tile.y = min(current_tile.y + 1, level_length - 1)
@@ -54,14 +87,13 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("exit"):
 		save_level()
 		get_tree().change_scene_to_file("res://Scenes/Menus/level_selector.tscn")
-		
 
 func insert_object(scene: PackedScene, id: int):
 	# Remove all nodes at that position already:
-	for child in NOTES.get_children():
+	for child in OBJECTS.get_children():
 		if child.name.begins_with(str(current_tile)):
 			child.queue_free()
-			NOTES.remove_child(child)
+			OBJECTS.remove_child(child)
 			break
 	
 	# Add the new node to the level data
@@ -76,8 +108,38 @@ func insert_object(scene: PackedScene, id: int):
 	var obj: Node = scene.instantiate()
 	obj.position = Vector3(current_tile.x * 10 - (current_level.lanes / 2) * 10, 1, current_tile.y * -10)
 	obj.name = str(current_tile) + obj.name
-	NOTES.add_child(obj)
+	OBJECTS.add_child(obj)
 
 func save_level():
 	current_level.data = level_data
+	current_level.lanes = len(level_data[0])
 	LevelLoader.save_level_data(current_level)
+
+func _on_add_lane_pressed(amount: Node) -> void:
+	var lanes = current_level.lanes
+	for i in range(len(level_data)):
+		level_data[i] = "0" + level_data[i] + "0"
+	
+	amount.text = str(lanes + 2)
+	current_tile.x = (lanes + 2) / 2
+	save_level()
+	reload_level()
+
+func _on_subtract_lane_pressed(amount: Node) -> void:
+	var lanes = current_level.lanes
+	if (lanes == 1):
+		return
+	for i in range(len(level_data)):
+		level_data[i] = level_data[i].substr(1, lanes - 2)
+		
+	amount.text = str(lanes - 2)
+	current_tile.x = (lanes - 2) / 2
+	
+	save_level()
+	reload_level()
+
+func _on_subtract_nps_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_add_nps_pressed() -> void:
+	pass # Replace with function body.
