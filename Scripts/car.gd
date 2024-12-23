601730ef1bb3166db46f317919952f784a63c83e
Calverin <38361803+Calverin@users.noreply.github.com>
2024-12-23 09:57:01 -0500

    Win Screen

--- Scripts/car.gd
+++ Scripts/car.gd
@@ -209,5 +209,5 @@ func win():
 	speed = 0
 	$CrashSound.play(0.0)
 	await get_tree().create_timer(1.5, false).timeout
-	get_tree().change_scene_to_file("res://Scenes/Menus/main_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Menus/win_screen.tscn")
 	
